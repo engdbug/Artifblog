403095f21e1756e05fa20cea600884f69e351a03
Jonathan Tyler Cummins <Jonathantylercumminstech@gmail.com>
2024-11-01 23:03:40 -0500

    Add files via upload

---
+++ Solar System project structure.php
@@ -0,0 +1,9 @@
+my_flask_app/
+├── Artifblog.py              # Your main Flask application file
+├── templates/
+│   └── Astronomy And Other Stuff.html      # Your HTML file
+├── static/
+├── static/
+│   ├── Solar-System.css
+│   ├── solar system3.jpeg
+│   └── solar_system_orbital_dark.png
